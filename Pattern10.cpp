0959a7e64812b2890b0970b2235e97fa167d502e
aditya82-uwu <79536657+aditya82-uwu@users.noreply.github.com>
2021-02-28 00:11:35 +0530

    Add files via upload

--- Pattern10.cpp
+++ Pattern10.cpp
@@ -2,19 +2,17 @@
 int main()
 {
 	int n=5;
-	
 	int i,j;
 	
 	for(i=0;i<n;i++)
-	{  
-		for(j=0;j<=i;j++)
+	{
+		for(j=0;j<i;j++)
 		{
 			printf(" ");
 		}
 		for(j=0;j<n-i;j++)
 		{
 			printf("* ");
-		}
-		printf("\n");
-	} return 0;
+		} printf("\n");
+	}
 }
